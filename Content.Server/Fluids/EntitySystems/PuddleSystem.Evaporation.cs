e93c63703a98a015adcdc90a5639549b56f9b86b
Dvir <39403717+dvir001@users.noreply.github.com>
2024-02-17 01:35:40 +0200

    Human BaseLine (#11)
    * BaseLine
    
    * More FIxing
    
    * Update animal.yml
    
    * Update animal.yml

--- Content.Server/Fluids/EntitySystems/PuddleSystem.Evaporation.cs
+++ Content.Server/Fluids/EntitySystems/PuddleSystem.Evaporation.cs
@@ -12,8 +12,13 @@ public sealed partial class PuddleSystem
 
     [ValidatePrototypeId<ReagentPrototype>]
     private const string Water = "Water";
+    private const string FluorosulfuricAcid = "FluorosulfuricAcid"; // SpaceFactory
+    private const string Vomit = "Vomit"; // SpaceFactory
+    private const string InsectBlood = "InsectBloodt"; // SpaceFactory
+    private const string AmmoniaBlood = "AmmoniaBlood"; // SpaceFactory
+    private const string ZombieBlood = "ZombieBlood"; // SpaceFactory
 
-    public static string[] EvaporationReagents = new[] { Water };
+    public static string[] EvaporationReagents = new[] { Water, Vomit, InsectBlood, AmmoniaBlood, ZombieBlood, Blood, Slime, CopperBlood, FluorosulfuricAcid }; // SpaceFactory
 
     private void OnEvaporationMapInit(Entity<EvaporationComponent> entity, ref MapInitEvent args)
     {
